9c85f4be756e4a875148b87141caa57bdf607768
seohyun-kim <selenium34@inu.ac.kr>
2022-09-15 20:53:16 +0900

    fix : 전처리 과정 일부 변경

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 ORIGIN_MODEL = "best_model_origin.pth"
 ESENTIAL_MODEL = "./faceshape_model.pth"
+ESENTIAL_MODEL_GRAY = "./faceshape_model_gray.pth"
+
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -25,7 +27,6 @@ class EffNet(nn.Module):
     def __init__(self, num_classes=5):
         super(EffNet, self).__init__()
         self.eff = EfficientNet.from_pretrained('efficientnet-b5', num_classes=num_classes, in_channels=1)
-        # self.eff = EfficientNet.from_pretrained('efficientnet-b5', num_classes=num_classes, in_channels=3)
     def forward(self, x):
         x = self.eff(x)
         return x
@@ -49,42 +50,41 @@ IMG_OBLONG = 'data_set/Oblong/oblong (6).jpg'
 IMG_OVAL = 'data_set/Oval/Oval (2).jpg'
 IMG_ROUND = 'data_set/Round/round (1).jpg'
 IMG_SQUARE = 'data_set/Square/square (8).jpg'
-IMG_YJ = 'data_set/yj.jpg'
-IMG_EB = 'data_set/eb2.jpg'
-IMG_KHD = 'data_set/khd.jpg'
+IMG_YJ = 'data_set/Round/round_yj.jpg'
+IMG_EB = 'data_set/Oblong/oblong_eb.jpg'
+IMG_KHD = 'data_set/Square/square_khd.jpg'
 ING_JB = 'data_set/Oval/oval_jb.jpg'
 
 # 여기에 타겟 이미지 작성
-target_img = cv2.imread(IMG_HEART)
-
+target_img = cv2.imread(IMG_YJ)
 
 # 이미지 전처리
-print(target_img.shape)
-
 gray = cv2.cvtColor(target_img, cv2.COLOR_BGR2GRAY)  # gray scale
-
-print(gray.shape)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # 얼굴 찾기
 for (x, y, w, h) in faces:
-    cv2.rectangle(target_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    cropped = target_img[y: y + h, x: x + w]
-    resized = cv2.resize(cropped, (200, 200))
+    # cv2.rectangle(target_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # cropped = target_img[y: y + h, x: x + w]
+    # resized = cv2.resize(cropped, (200, 200))
 
-    img_yuv = cv2.cvtColor(resized, cv2.COLOR_BGR2YUV)
+    cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cropped = gray[y: y + h, x: x + w]
+    equalized = cv2.equalizeHist(cropped)
+    # img_yuv = cv2.cvtColor(resized, cv2.COLOR_BGR2YUV)
+    # img_yuv = cv2.cvtColor(cropped, cv2.COLOR_BGR2YUV)
 
     # equalize the histogram of the Y channel
-    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+    # img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
 
     # convert the YUV image back to RGB format
-    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+    # img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
     # edge enhancement
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-    pre_processed_img = cv2.filter2D(src=img_output, ddepth=-1, kernel=kernel)
-    pre_processed_img = cv2.cvtColor(pre_processed_img, cv2.COLOR_BGR2GRAY)
+    pre_processed_img = cv2.filter2D(src=equalized, ddepth=-1, kernel=kernel)
+    # pre_processed_img = cv2.cvtColor(pre_processed_img, cv2.COLOR_BGR2GRAY)
 
 # 텐서화
 convert_tensor = transforms.ToTensor()
@@ -93,11 +93,7 @@ processed_img = convert_tensor(pre_processed_img)
 # processed_img = convert_tensor(target_img)
 
 with torch.no_grad():
-    print(processed_img.shape)
     inputs = torch.FloatTensor(processed_img.unsqueeze(0))
-    print(inputs.shape)
-    # inputs = inputs.unsqueeze(0)
-    print(inputs.shape)
     output = model(inputs)
 
     print(output)
