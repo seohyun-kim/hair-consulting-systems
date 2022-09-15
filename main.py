a1933248b9dbb10296215a07e6067c2d5a679a71
seohyun-kim <selenium34@inu.ac.kr>
2022-09-15 16:55:44 +0900

    [ERROR] 채널 수 문제 RuntimeError: Given groups=1, weight of size [48, 1, 3, 3], expected input[1, 3, 201, 201] to have 1 channels, but got 3 channels instead

--- main.py
+++ main.py
@@ -8,19 +8,33 @@ from torchvision import transforms
 import torchvision.models as models
 
 
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 ORIGIN_MODEL = "best_model_origin.pth"
 ESENTIAL_MODEL = "./faceshape_model.pth"
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
 
-#model = EfficientNet.from_pretrained('efficientnet-b5', num_classes=5)
-model = EfficientNet.from_pretrained("efficientnet-b5", in_channels=3, advprop=True)
-model.load_state_dict(torch.load(ESENTIAL_MODEL, map_location=device), strict=False)
-    # strict : 엄격하게 적용할 지 여부
+# #model = EfficientNet.from_pretrained('efficientnet-b5', num_classes=5)
+# model = EfficientNet.from_pretrained("efficientnet-b5", in_channels=3, advprop=True)
+# model.load_state_dict(torch.load(ESENTIAL_MODEL, map_location=device), strict=False)
+#     # strict : 엄격하게 적용할 지 여부
 
+
+class EffNet(nn.Module):
+    def __init__(self, num_classes=5):
+        super(EffNet, self).__init__()
+        self.eff = EfficientNet.from_pretrained('efficientnet-b5', num_classes=num_classes, in_channels=1)
+    def forward(self, x):
+        x = self.eff(x)
+        return x
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = EffNet().to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+model.load_state_dict(torch.load(ESENTIAL_MODEL, map_location=device), strict=True)
 
 model.eval()  # eval 모드로 설정
 
@@ -29,19 +43,22 @@ model.eval()  # eval 모드로 설정
 
 shape_class = {0: "heart", 1: "oblong", 2: "oval", 3: "round", 4: "square"}
 
-IMG_HEART = 'data_set/Heart/heart (3).jpg'  # 0 (맞음)
-IMG_OBLONG = 'data_set/Oblong/oblong (6).jpg'  # 4 (틀림)
-IMG_OVAL = 'data_set/Oval/Oval (2).jpg'  # 3 (맞음)
-IMG_ROUND = 'data_set/Round/round (1).jpg'  # 2 (틀림)
-IMG_SQUARE = 'data_set/Square/square (8).jpg'  # 1 (틀림)
-IMG_YJ = 'data_set/yj.jpg'  # 1 (유진이)
-IMG_EB = 'data_set/eb2.jpg'  # 3 (박은빈)
-IMG_KHD = 'data_set/khd.jpg'  # 2 Oblong (강호동)
+IMG_HEART = 'data_set/Heart/heart (3).jpg'
+IMG_OBLONG = 'data_set/Oblong/oblong (6).jpg'
+IMG_OVAL = 'data_set/Oval/Oval (2).jpg'
+IMG_ROUND = 'data_set/Round/round (1).jpg'
+IMG_SQUARE = 'data_set/Square/square (8).jpg'
+IMG_YJ = 'data_set/yj.jpg'
+IMG_EB = 'data_set/eb2.jpg'
+IMG_KHD = 'data_set/khd.jpg'
+ING_JB = 'data_set/Oval/oval_jb.jpg'
 
 # 여기에 타겟 이미지 작성
-target_img = cv2.imread(IMG_YJ)
+target_img = cv2.imread(IMG_HEART)
+
 
 # 이미지 전처리
+
 gray = cv2.cvtColor(target_img, cv2.COLOR_BGR2GRAY)  # gray scale
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # 얼굴 찾기
 for (x, y, w, h) in faces:
@@ -67,13 +84,17 @@ for (x, y, w, h) in faces:
 convert_tensor = transforms.ToTensor()
 
 processed_img = convert_tensor(pre_processed_img)
+# processed_img = convert_tensor(target_img)
 
 with torch.no_grad():
-
+    print(processed_img.size())
     inputs = torch.FloatTensor(processed_img.unsqueeze(0))
     print(inputs.size())
+    # inputs = inputs.unsqueeze(0)
+    print(inputs.size())
     output = model(inputs)
+
     print(output)
     pred_output = output.argmax(dim=1, keepdim=True)
-    print(pred_output%5) # ㅋㅋ 에잇 몰라!
+    print(pred_output)
    # print(shape_class[int(pred)])
