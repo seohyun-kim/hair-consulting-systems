f9832d75cf3eec3296989311b5fb0278528f4466
seohyun-kim <selenium34@inu.ac.kr>
2022-09-15 21:00:32 +0900

    chore : 불 필요한 부분 제거 및 실행 결과 작성

--- main.py
+++ main.py
@@ -40,57 +40,41 @@ model.load_state_dict(torch.load(ESENTIAL_MODEL, map_location=device), strict=Tr
 
 model.eval()  # eval 모드로 설정
 
-# for param in model.parameters():
-#     print(param)
-
 shape_class = {0: "heart", 1: "oblong", 2: "oval", 3: "round", 4: "square"}
 
-IMG_HEART = 'data_set/Heart/heart (3).jpg'
-IMG_OBLONG = 'data_set/Oblong/oblong (6).jpg'
-IMG_OVAL = 'data_set/Oval/Oval (2).jpg'
-IMG_ROUND = 'data_set/Round/round (1).jpg'
-IMG_SQUARE = 'data_set/Square/square (8).jpg'
-IMG_YJ = 'data_set/Round/round_yj.jpg'
-IMG_EB = 'data_set/Oblong/oblong_eb.jpg'
-IMG_KHD = 'data_set/Square/square_khd.jpg'
-ING_JB = 'data_set/Oval/oval_jb.jpg'
+IMG_HEART = 'data_set/Heart/heart (3).jpg' # oblong
+IMG_OBLONG = 'data_set/Oblong/oblong (9).jpg' # oblong
+IMG_OVAL = 'data_set/Oval/Oval (2).jpg' # oval
+IMG_ROUND = 'data_set/Round/round (1).jpg' # oval
+IMG_SQUARE = 'data_set/Square/square (8).jpg' # square
+IMG_MINA = 'data_set/Heart/heart_mina.jpg' # round
+IMG_YJ = 'data_set/Round/round_yj.jpg' # round
+IMG_EB = 'data_set/Oblong/oblong_eb.jpg' # oblong
+IMG_KHD = 'data_set/Square/square_khd.jpg' # square
+ING_JB = 'data_set/Oval/oval_jb.jpg' # round
 
 # 여기에 타겟 이미지 작성
-target_img = cv2.imread(IMG_YJ)
+target_img = cv2.imread(IMG_MINA)
 
 # 이미지 전처리
 gray = cv2.cvtColor(target_img, cv2.COLOR_BGR2GRAY)  # gray scale
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # 얼굴 찾기
 for (x, y, w, h) in faces:
-    # cv2.rectangle(target_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    # cropped = target_img[y: y + h, x: x + w]
-    # resized = cv2.resize(cropped, (200, 200))
-
     cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
     cropped = gray[y: y + h, x: x + w]
     equalized = cv2.equalizeHist(cropped)
-    # img_yuv = cv2.cvtColor(resized, cv2.COLOR_BGR2YUV)
-    # img_yuv = cv2.cvtColor(cropped, cv2.COLOR_BGR2YUV)
-
-    # equalize the histogram of the Y channel
-    # img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-
-    # convert the YUV image back to RGB format
-    # img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
     # edge enhancement
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
     pre_processed_img = cv2.filter2D(src=equalized, ddepth=-1, kernel=kernel)
-    # pre_processed_img = cv2.cvtColor(pre_processed_img, cv2.COLOR_BGR2GRAY)
 
 # 텐서화
 convert_tensor = transforms.ToTensor()
 
 processed_img = convert_tensor(pre_processed_img)
-# processed_img = convert_tensor(target_img)
 
 with torch.no_grad():
     inputs = torch.FloatTensor(processed_img.unsqueeze(0))
@@ -99,4 +83,4 @@ with torch.no_grad():
     print(output)
     pred_output = output.argmax(dim=1, keepdim=True)
     print(pred_output)
-   # print(shape_class[int(pred)])
+    print(shape_class[int(pred_output)])
